2eff9c14953d460830a46330a6041b379ce8d127
Finite <33845166+finia2NA@users.noreply.github.com>
2024-04-30 16:47:18 +0200

    get and parse XML, tests

---
+++ lib/src/tests/getter.test.ts
@@ -0,0 +1,52 @@
+import { getWholeDataAsJson } from "../getter";
+
+describe('getter', () => {
+  let data: any;
+
+  beforeAll(async () => {
+    // We're doing this here so we just have to do it once
+    data = await getWholeDataAsJson();
+  });
+
+  test('data should be defined', () => {
+    expect(data).toBeDefined();
+    console.log(data);
+  });
+
+  test('data should have a service list of expected structure', () => {
+    /**
+     * We expect:
+     * data
+     * - ServiceList
+     *   - Name
+     *   - ProviderName
+     *   - ContentGuideSourceList
+     *     - ContentGuideSource[]
+     *   - LCNTableList
+     *     - LCNTable[]
+     *   - RegionList
+     *     - Region[]
+     *   - Service[]
+     */
+
+    expect(data).toHaveProperty('ServiceList');
+
+    expect(data.ServiceList).toHaveProperty('Name');
+    expect(data.ServiceList).toHaveProperty('ProviderName');
+
+    expect(data.ServiceList).toHaveProperty('ContentGuideSourceList');
+    expect(data.ServiceList.ContentGuideSourceList).toHaveProperty('ContentGuideSource');
+    expect(data.ServiceList.ContentGuideSourceList.ContentGuideSource).toBeInstanceOf(Array);
+
+    expect(data.ServiceList).toHaveProperty('LCNTableList');
+    expect(data.ServiceList.LCNTableList).toHaveProperty('LCNTable');
+    expect(data.ServiceList.LCNTableList.LCNTable).toBeInstanceOf(Array);
+
+    expect(data.ServiceList).toHaveProperty('RegionList');
+    expect(data.ServiceList.RegionList).toHaveProperty('Region');
+    expect(data.ServiceList.RegionList.Region).toBeInstanceOf(Array);
+
+    expect(data.ServiceList).toHaveProperty('Service');
+    expect(data.ServiceList.Service).toBeInstanceOf(Array);
+  });
+});

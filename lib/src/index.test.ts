7408b645233cd8ee12c531dca437e1446beaf6be
Finite <33845166+finia2NA@users.noreply.github.com>
2024-05-02 02:27:35 +0200

    fixes for tests

--- lib/src/index.test.ts
+++ lib/src/index.test.ts
@@ -6,13 +6,13 @@ describe("DVBI", () => {
   });
 
   it("should have a refreshData method", () => {
-    const dvbi = new DVBI();
+    const dvbi = DVBI.getInstance();
     expect(dvbi.refreshData).toBeDefined();
   });
 
   // after construction, the data should be not null
   it("should have data after init call", async () => {
-    const dvbi = new DVBI();
+    const dvbi = DVBI.getInstance();
     await dvbi.init();
     expect(dvbi.data).toBeDefined();
   });

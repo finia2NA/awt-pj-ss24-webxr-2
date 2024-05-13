74abda793782b6872a8f32de4408c172c59d165a
Finite <33845166+finia2NA@users.noreply.github.com>
2024-05-14 00:41:15 +0200

    Add UI element Component, Update Stories

--- app/src/components/XRWindow.tsx
+++ app/src/components/XRWindow.tsx
@@ -1,19 +1,21 @@
 import { ReactNode } from 'react';
 
+/**
+ * Props for the XRWindow component.
+ */
 export interface XRWindowProps {
+  /**
+   * The content of the XRWindow.
+   */
   children: ReactNode;
 }
 
 const XRWindow = ({ children }: XRWindowProps) => {
   return (
-    <div className="bg-mainBG
-    dark:bg-dark-mainBG
-    p-16
-    pt-12
-    pl-14
+    <div className="bg-mainBG dark:bg-dark-mainBG
+    text-primary dark:text-dark-primary
+    p-16 pt-12 pl-14
     rounded-lg
-    text-primary
-    dark:text-dark-primary
     ">
       {children}
     </div>

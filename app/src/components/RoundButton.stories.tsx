bc2fbaa8592a18332629dc9d5fe0f2b1560a16dc
Finite <33845166+finia2NA@users.noreply.github.com>
2024-05-13 22:32:29 +0200

    Add RoundButton and XRWindow UI Elements

---
+++ app/src/components/RoundButton.stories.tsx
@@ -0,0 +1,44 @@
+// RoundButton.stories.tsx
+import type { Meta, StoryObj } from '@storybook/react';
+import RoundButton from './RoundButton';
+
+
+const meta = {
+  title: 'Components/RoundButton',
+  component: RoundButton,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+  },
+} satisfies Meta<typeof RoundButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const Default: Story = {
+  args: {
+    children: 'Button Text',
+  },
+};
+
+export const Active: Story = {
+  args: {
+    children: 'Button Text',
+    active: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    children: 'Button Text',
+    disabled: true,
+  },
+};
+
+// export const Empty: Story = {
+//   args: {
+//     children: '',
+//   },
+// };
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,9 +4,23 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'media',
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    extend: {
+      colors: {
+        mainBG: "rgba(204, 204, 204, 0.7)",
+        primary: '#000000',
+        highlight: '#26B5EA',
+        buttonBG: '#EEEEEE',
+        uiElem: 'rgba(116, 115, 115, 0.61)',
+        dark: {
+          mainBG: 'rgba(4, 4, 4, 0.7)',
+          primary: '#FFFFFF',
+          buttonBG: '#444444',
+        }
+      },
+    },
+    plugins: [],
+  }
+} // Add this closing curly brace
 

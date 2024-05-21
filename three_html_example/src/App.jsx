07b11eae3f313dfa062e46e8e09e503d40803248
Finite <33845166+finia2NA@users.noreply.github.com>
2024-05-21 15:50:32 +0200

    Add example of lifted state to three mvp

--- three_html_example/src/App.jsx
+++ three_html_example/src/App.jsx
@@ -5,27 +5,39 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Html } from '@react-three/drei';
+import PropTypes from 'prop-types';
 
-const ExampleReactComponent = () => {
-  const [count, setCount] = useState(0)
+
+
+
+const ExampleReactComponent = ({ count, setCount }) => {
+  const [stateCount, setStateCount] = useState(0)
 
   return (
     <>
       <h3>React Example Component</h3>
       <img src={reactLogo} alt="React Logo" />
       <img src={viteLogo} alt="Vite Logo" />
-      <button onClick={() => setCount((count) => count + 1)}>Count: {count}</button>
+      <button onClick={() => setCount((c) => c + 1)}> Shared Count: {count}</button>
+      <button onClick={() => setStateCount((c => c + 1))}> State Count: {stateCount}</button>
     </>
   )
 }
 
+// This is how you do it in React JS, in TS you would use an interface to define the props
+ExampleReactComponent.propTypes = {
+  count: PropTypes.number.isRequired,
+  setCount: PropTypes.func.isRequired
+}
+
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <h1>Components in a vacuum</h1>
-        <ExampleReactComponent />
+        <ExampleReactComponent count={count} setCount={setCount} />
       </div>
       <div>
         <h1>Components in three</h1>
@@ -36,7 +48,7 @@ function App() {
           <OrbitControls />
           <ambientLight />
           <Html position={[0, 0.5, 2]} transform>
-            <ExampleReactComponent />
+            <ExampleReactComponent count={count} setCount={setCount} />
           </Html>
         </Canvas>
       </div>

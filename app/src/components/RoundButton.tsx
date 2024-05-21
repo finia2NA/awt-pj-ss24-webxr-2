33e0553eb11dea532ddb6b73dfdc04378801449c
Finite <33845166+finia2NA@users.noreply.github.com>
2024-05-21 17:06:19 +0200

    Add Button Group Component

--- app/src/components/RoundButton.tsx
+++ app/src/components/RoundButton.tsx
@@ -30,10 +30,9 @@ const RoundButton = (props: RoundButtonProps) => {
   const children = props.children;
 
   return (
-    // TODO: border should be 1 when inactive but hovered, this does not work for some reason
-    <button className={`font-bold px-4 rounded-full  text-primary dark:text-dark-primary bg-buttonBG dark:bg-dark-buttonBG hover:border-primary hover:dark:border-dark-primary hover:scale-105 transition-transform
+    <button className={`font-semibold px-4 rounded-full  text-primary dark:text-dark-primary bg-buttonBG dark:bg-dark-buttonBG hover:border-primary hover:dark:border-dark-primary hover:scale-105 transition-transform
     focus:outline-none
-    ${active ? 'border-4' : 'border-1 border-transparent hover:border-primary hover:dark:border-dark-primary'}
+    ${active ? 'border-2' : 'border-1 border-transparent hover:border-primary hover:dark:border-dark-primary'}
     ${disabled ? 'opacity-50 cursor-not-allowed' : ''}
 
     border-primary  dark:border-dark-primary

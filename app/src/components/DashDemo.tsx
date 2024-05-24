dc467f47306fad87237f9b5b9234a6743cd9f1d6
Guchen <guchengit@users.noreply.github.com>
2024-05-24 14:50:20 +0200

    Added ability to seek and change playback rate with ambient player

--- app/src/components/DashDemo.tsx
+++ app/src/components/DashDemo.tsx
@@ -6,9 +6,10 @@ const DashDemo = () => {
     const [isPaused, setPaused] = useState<boolean>(false);
     const [isMuted, setMuted] = useState<boolean>(true);
     const [volume, setVolume] = useState<number>(1);
-    // TODO: Add playback sync between the two players
+    const [playbackTime, setPlaybackTime] = useState<number>(0);
+    const [playbackRate, setPlaybackRate] = useState<number>(1);
 
-    // TODO: Change this for the background player
+
     function handlePlaybackUpdate(eventType: DashPlayerEvents, data?: DashPlayerEventData) {
         switch (eventType) {
             case DashPlayerEvents.MUTED:
@@ -28,6 +29,16 @@ const DashDemo = () => {
                 setVolume(data!.data as number);
                 console.log(`Volume changed to: ${data!.data}`)
                 break;
+            case DashPlayerEvents.PLAYBACK_SEEKING:
+                if (!data || data?.type !== 'time') return;
+                console.log(`Time updated to: ${data!.data}`)
+                setPlaybackTime(data!.data as number);
+                break;
+            case DashPlayerEvents.PLAYBACK_RATE_CHANGED:
+                if (!data || data?.type !== 'rate') return;
+                console.log(`Playback rate changed to: ${data!.data}`)
+                setPlaybackRate(data!.data as number);
+                break;
             default:
                 // Handle any other cases or do nothing
                 break;
@@ -39,7 +50,7 @@ const DashDemo = () => {
             <button onClick={() => setPaused(!isPaused)}>Pause / unpause</button>
             <button onClick={() => setMuted(!isMuted)}>Mute / unmute</button>
             <div className="w-[800px] h-[500px] relative">
-                <DashPlayer className="absolute z-10 w-full h-auto top-0 bottom-0 left-0 right-0 m-auto blur-3xl" src="https://dash.akamaized.net/dash264/TestCasesIOP33/adapatationSetSwitching/5/manifest.mpd" paused={isPaused} muted={true} handlePlaybackUpdate={handlePlaybackUpdate} />
+                <DashPlayer className="absolute z-10 w-full h-auto top-0 bottom-0 left-0 right-0 m-auto blur-[100px]" src="https://dash.akamaized.net/dash264/TestCasesIOP33/adapatationSetSwitching/5/manifest.mpd" paused={isPaused} muted={true} handlePlaybackUpdate={handlePlaybackUpdate} onlyControlled={true} playbackTime={playbackTime} playbackRate={playbackRate} controls={false} />
                 <DashPlayer className="absolute z-20 w-11/12 h-auto top-0 bottom-0 left-0 right-0 m-auto" src="https://dash.akamaized.net/dash264/TestCasesIOP33/adapatationSetSwitching/5/manifest.mpd" paused={isPaused} muted={isMuted} handlePlaybackUpdate={handlePlaybackUpdate} />
             </div>
         </>
--- app/src/components/DashPlayer.tsx
+++ app/src/components/DashPlayer.tsx
@@ -29,13 +29,29 @@ export interface DashPlayerProps {
    * Custom class names for the video element (for Tailwind)
    */
   className?: string;
+  /**
+   * Whether the video should be only controlled by the parent component
+   */
+  onlyControlled?: boolean;
+  /**
+   * The current playback time of the video
+   * Should only be included when onlyControlled is true
+   * and this component effectively is a slave to another DashPlayer component (ambient player)
+   */
+  playbackTime?: number;
+  /**
+   * The playback rate of the video
+   * Should only be included when onlyControlled is true
+   * and this component effectively is a slave to another DashPlayer component (ambient player)
+   */
+  playbackRate?: number;
 }
 
 /**
  * A simple DASH player component that uses the dash.js library.
  * Note that the `paused` prop is also used for the autoplay feature upon initialization.
  */
-const DashPlayer = ({ src, paused = true, controls = true, muted = false, handlePlaybackUpdate = () => { }, className = '' }: DashPlayerProps) => {
+const DashPlayer = ({ src, paused = true, controls = true, muted = false, handlePlaybackUpdate = () => { }, className = '', onlyControlled = false, playbackTime = 0, playbackRate = 1 }: DashPlayerProps) => {
   /**
    * Ref to the actual video element that will be used by the dash.js player
    */
@@ -51,41 +67,63 @@ const DashPlayer = ({ src, paused = true, controls = true, muted = false, handle
       playerRef.current.initialize(videoRef.current, src, !paused);
       playerRef.current.setMute(muted);
 
-      /*
-      --------------------------------
-      Handle events emitted by the player
-      --------------------------------
-      */
-      // This event is emitted when the player encounters an error
-      // (e.g. the video file is not found or the manifest is invalid)
-      // Maybe we should handle this event in a more user-friendly way
-      playerRef.current.on(dashjs.MediaPlayer.events.ERROR, (e) => {
-        console.error('Dash.js error:', e);
-      });
+      if (!onlyControlled) {
+        /*
+        --------------------------------
+        Handle events emitted by the player
+        --------------------------------
+        */
+        // This event is emitted when the player encounters an error
+        // (e.g. the video file is not found or the manifest is invalid)
+        // Maybe we should handle this event in a more user-friendly way
+        playerRef.current.on(dashjs.MediaPlayer.events.ERROR, (e) => {
+          console.error('Dash.js error:', e);
+        });
 
-      playerRef.current.on(dashjs.MediaPlayer.events.PLAYBACK_PAUSED, () => {
-        handlePlaybackUpdate(DashPlayerEvents.PLAYBACK_PAUSED)
-      });
+        playerRef.current.on(dashjs.MediaPlayer.events.PLAYBACK_PAUSED, () => {
+          handlePlaybackUpdate(DashPlayerEvents.PLAYBACK_PAUSED)
+        });
 
-      // We use PLAYBACK_PLAYING instead of PLAYBACK_STARTED as the latter is only
-      // emitted when playback is resumed after a prior pause event
-      playerRef.current.on(dashjs.MediaPlayer.events.PLAYBACK_PLAYING, () => {
-        handlePlaybackUpdate(DashPlayerEvents.PLAYBACK_PLAYING)
-      });
+        // We use PLAYBACK_PLAYING instead of PLAYBACK_STARTED as the latter is only
+        // emitted when playback is resumed after a prior pause event
+        playerRef.current.on(dashjs.MediaPlayer.events.PLAYBACK_PLAYING, () => {
+          handlePlaybackUpdate(DashPlayerEvents.PLAYBACK_PLAYING);
+        });
 
-      playerRef.current.on(dashjs.MediaPlayer.events.PLAYBACK_VOLUME_CHANGED, () => {
-        // PLAYBACK_VOLUME_CHANGED is emitted when the volume is changed in any way, so also when
-        // the video is muted or unmuted. This has to be detected manually however.
-        handlePlaybackUpdate(playerRef.current?.isMuted() ? DashPlayerEvents.MUTED : DashPlayerEvents.UNMUTED);
+        playerRef.current.on(dashjs.MediaPlayer.events.PLAYBACK_VOLUME_CHANGED, () => {
+          // PLAYBACK_VOLUME_CHANGED is emitted when the volume is changed in any way, so also when
+          // the video is muted or unmuted. This has to be detected manually however.
+          handlePlaybackUpdate(playerRef.current?.isMuted() ? DashPlayerEvents.MUTED : DashPlayerEvents.UNMUTED);
 
-        // Send the updated volume to the parent component
-        const updateData: DashPlayerEventData = {
-          type: 'volume',
-          data: playerRef.current!.getVolume()
-        }
-        handlePlaybackUpdate(DashPlayerEvents.VOLUME_CHANGED, updateData)
-      });
+          // Send the updated volume to the parent component
+          const updateData: DashPlayerEventData = {
+            type: 'volume',
+            data: playerRef.current!.getVolume()
+          }
+          handlePlaybackUpdate(DashPlayerEvents.VOLUME_CHANGED, updateData);
+        });
 
+        // We don't use PLAYBACK_TIME_UPDATED since it is called too infrequently
+        // and makes the background player lag
+        // We also don't use PLAYBACK_SEEKED since it doesn't contain
+        // any information about the new time
+        playerRef.current.on(dashjs.MediaPlayer.events.PLAYBACK_SEEKING, (e) => {
+          const updateData: DashPlayerEventData = {
+            type: 'time',
+            data: e.seekTime!
+          }
+          handlePlaybackUpdate(DashPlayerEvents.PLAYBACK_SEEKING, updateData);
+        });
+
+        playerRef.current.on(dashjs.MediaPlayer.events.PLAYBACK_RATE_CHANGED, (e) => {
+          const updateData: DashPlayerEventData = {
+            type: 'rate',
+            data: e.playbackRate!
+          }
+          handlePlaybackUpdate(DashPlayerEvents.PLAYBACK_RATE_CHANGED, updateData);
+        });
+
+      }
       // --------------------------------
 
       // This is a cleanup function that gets called when the component unmounts.
@@ -108,6 +146,18 @@ const DashPlayer = ({ src, paused = true, controls = true, muted = false, handle
       }
     }
   }, [muted, paused]);
+
+  useEffect(() => {
+    if (playerRef.current) {
+      playerRef.current.seek(playbackTime);
+    }
+  }, [playbackTime]);
+
+  useEffect(() => {
+    if (playerRef.current) {
+      playerRef.current.setPlaybackRate(playbackRate);
+    }
+  }, [playbackRate]);
 
   // The video element itself
   return (

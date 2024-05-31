8747ed61db6b59416abfb6f753fc61067a9d4b1a
Finite <33845166+finia2NA@users.noreply.github.com>
2024-05-31 13:09:49 +0200

    Update dropdown story to match dropdown 2 improved story

--- app/src/components/Dropdown.stories.tsx
+++ app/src/components/Dropdown.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Dropdown from './Dropdown';
+import XRWindow from './XRWindow';
 
 const meta = {
   title: 'Components/Dropdown',
@@ -16,7 +17,19 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        items: ['Berlin', 'Hamburg', 'Munich', 'Cologne', 'Frankfurt'],
-    },
+  args: {
+    items: ['Berlin', 'Hamburg', 'Munich', 'Cologne', 'Frankfurt'],
+  },
+
+  render: (args) => {
+
+    return (
+      <XRWindow>
+        <div className='flex flex-col items-center space-y-3'>
+          <Dropdown {...args} />
+          <span>This is some text that is rendered below the dropdown. The intended behaviour is that the dropdown expands over this text.</span>
+        </div>
+      </XRWindow>
+    );
+  }
 };

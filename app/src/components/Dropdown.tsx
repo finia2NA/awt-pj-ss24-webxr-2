1af0a560fbba17992e4e6904806abdaa5556af8a
Finite <33845166+finia2NA@users.noreply.github.com>
2024-05-31 13:01:59 +0200

    Add animated chevron to dropdown

--- app/src/components/Dropdown.tsx
+++ app/src/components/Dropdown.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import useDarkMode from "../hooks/useDarkmode";
+import chevronDownIcon from "../assets/glyphs/chevron.down.svg";
+
 
 export interface DropdownProps {
     items: string[];
@@ -7,6 +10,8 @@ export interface DropdownProps {
 const Dropdown = ({ items }: DropdownProps) => {
     const [isOpen, setIsOpen] = useState(false);
     const [selected, setSelected] = useState(items[0]);
+    const isDarkMode = useDarkMode();
+
 
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
@@ -19,54 +24,49 @@ const Dropdown = ({ items }: DropdownProps) => {
 
     return (
         <div className="relative inline-block text-left">
-            <div>
+            <div className="flex flex-row items-center justify-center align-middle">
                 <button
                     type="button"
-                    className="inline-flex justify-between w-full rounded-3xl border border-gray-300 shadow-sm px-4 py-2 bg-darkerUIElem text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="inline-flex justify-between w-full rounded-3xl border items-center border-gray-300 shadow-sm px-4 py-2 bg-darkerUIElem text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     id="menu-button"
                     aria-expanded="true"
                     aria-haspopup="true"
                     onClick={toggleDropdown}
                 >
                     {selected}
-                    <svg
-                        className="-mr-1 ml-2 h-5 w-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clipRule="evenodd"
-                        />
-                    </svg>
+                    <img src={chevronDownIcon} alt="chevron down" className="ml-2" style={{
+                        WebkitFilter: isDarkMode ? 'invert(1)' : 'invert(0)',
+                        filter: isDarkMode ? 'invert(1)' : 'invert(0)',
+                        transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)'
+                    }} />
+
                 </button>
             </div>
 
-            {isOpen && (
-                <div
-                    className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="menu-button"
-                >
-                    <div className="py-1" role="none">
-                        {items.map((item: string, index: number) => (
-                            <a
-                                key={index}
-                                className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 cursor-pointer"
-                                role="menuitem"
-                                onClick={() => handleSelect(item)}
-                            >
-                                {item}
-                            </a>
-                        ))}
+            {
+                isOpen && (
+                    <div
+                        className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10"
+                        role="menu"
+                        aria-orientation="vertical"
+                        aria-labelledby="menu-button"
+                    >
+                        <div className="py-1" role="none">
+                            {items.map((item: string, index: number) => (
+                                <a
+                                    key={index}
+                                    className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 cursor-pointer"
+                                    role="menuitem"
+                                    onClick={() => handleSelect(item)}
+                                >
+                                    {item}
+                                </a>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            )}
-        </div>
+                )
+            }
+        </div >
     );
 };
 

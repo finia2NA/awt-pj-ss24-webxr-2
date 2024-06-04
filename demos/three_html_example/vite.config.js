683784620507e34524368f3e3d30880add02cc79
Finite <33845166+finia2NA@users.noreply.github.com>
2024-06-04 14:11:36 +0200

    fix ssl

--- demos/three_html_example/vite.config.js
+++ demos/three_html_example/vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// Use if you want to test the app on another device (local network)
-// WebXR only works on localhost or https! Only intended for testing purposes, not production
-// Also, --host needs to be set as an option for vite, see dev-host in package.json
-//import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import selfsigned from 'selfsigned';
+
+const attrs = [{ name: 'commonName', value: 'localhost' }];
+const pems = selfsigned.generate(attrs, { days: 365, keySize: 2048 });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()/*, basicSsl()*/]
-})
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000, // You can change this to any available port you prefer
+    https: {
+      key: pems.private,
+      cert: pems.cert,
+    },
+  },
+});

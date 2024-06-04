f81171a633ce240654d369b24ffb6ae35947ce2a
Finite <33845166+finia2NA@users.noreply.github.com>
2024-06-04 20:22:03 +0200

    Add storybook, new tab selector

---
+++ app2/src/components/Tabs.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Tabs, { Tab } from './Tabs';
+import { Canvas } from '@react-three/fiber';
+import { Fullscreen, Root } from '@react-three/uikit';
+import StoryHelper from './StoryHelper';
+
+const meta: Meta<typeof Tabs> = {
+  title: 'Windows/Tabs',
+  component: Tabs,
+  tags: ["autodocs"],
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    collapsed: false,
+    selectedTab: Tab.HOME,
+    // eslint-disable-next-line no-unused-vars
+    setSelectedTab: (tab: string) => { console.log("Selected: " + tab) }
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+
+  render: (args) =>
+    <>
+      <StoryHelper>
+        <Tabs {...args} />
+      </StoryHelper >
+    </>
+};
---
+++ app2/src/components/Tabs.tsx
@@ -0,0 +1,60 @@
+import { TabBar, TabBarItem } from './apfel/tab-bar'
+import { Text } from '@react-three/uikit'
+import { BoxSelect } from '@react-three/uikit-lucide'
+
+import { HomeIcon, SettingsIcon, Tv2Icon, LayoutListIcon } from '@react-three/uikit-lucide';
+import { ButtonType } from "./GlyphButton";
+
+import React from 'react';
+
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line react-refresh/only-export-components
+export enum Tab {
+  HOME,
+  TV,
+  GUIDE,
+  SETTINGS,
+}
+
+const getIcon = (tab: Tab) => {
+  switch (tab) {
+    case Tab.HOME:
+      return ButtonType.Home;
+    case Tab.TV:
+      return ButtonType.TV;
+    case Tab.GUIDE:
+      return ButtonType.Guide;
+    case Tab.SETTINGS:
+      return ButtonType.Settings;
+    default:
+      return ButtonType.ChevronDown;
+  }
+}
+
+export interface TabSelectorProps {
+  selectedTab: Tab;
+  setSelectedTab: (tab: Tab) => void;
+}
+
+
+const Tabs: React.FC = () => {
+  return (
+    // Todo: Specify the height
+    <TabBar defaultValue='Home' height={230}>
+      <TabBarItem value='Home' icon={<HomeIcon />}>
+        <Text>Home</Text>
+      </TabBarItem>
+      <TabBarItem value='TV' icon={<Tv2Icon />}>
+        <Text>TV</Text>
+      </TabBarItem>
+      <TabBarItem value='Guide' icon={<LayoutListIcon />}>
+        <Text>Guide</Text>
+      </TabBarItem>
+      <TabBarItem value='Settings' icon={<SettingsIcon />}>
+        <Text>Settings</Text>
+      </TabBarItem>
+    </TabBar>
+  );
+};
+
+export default Tabs;

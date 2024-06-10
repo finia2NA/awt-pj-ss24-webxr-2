9ba186c6dbbdfb40afa171393e9c3ccabad2f302
Finite <33845166+finia2NA@users.noreply.github.com>
2024-06-11 00:53:33 +0200

    Change way ssl cert is generated in uikit demo

--- demos/uikit-dash/vite.config.js
+++ demos/uikit-dash/vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import selfsigned from 'selfsigned';
 
+const attrs = [{ name: 'commonName', value: 'localhost' }];
+const pems = selfsigned.generate(attrs, { days: 365, keySize: 2048 });
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()],
   server: {
-    https: true, // Enable HTTPS
-    host: true, // Expose to network
+    host: '0.0.0.0',
+    port: 3000, // You can change this to any available port you prefer
+    https: {
+      key: pems.private,
+      cert: pems.cert,
+    },
   },
-})
+});

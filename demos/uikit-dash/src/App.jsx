b46caa47661218812891f73bc9591140713f4b3b
Guchen <guchengit@users.noreply.github.com>
2024-06-11 09:15:46 +0200

    Moved things, removed unnecessary files, more documentation

--- demos/uikit-dash/src/App.jsx
+++ demos/uikit-dash/src/App.jsx
@@ -11,7 +11,7 @@ import {
 } from "@coconut-xr/natuerlich/react";
 import { OrbitControls } from "@react-three/drei";
 import { Root, Text } from "@react-three/uikit";
-import { DashVideo } from "./components/default/customVideo";
+import { DashVideo } from "./components/custom/customVideo";
 
 const sessionOptions = {
   requiredFeatures: ["local-floor", "hand-tracking"]
@@ -33,12 +33,18 @@ export default function Index() {
       <button onClick={enterVR}>Enter VR</button>
       <XRCanvas>
     <OrbitControls />
-    <group position={[0, 1, 1]}>
-    <Root backgroundColor="red" sizeX={3} sizeY={2} flexDirection="row">
-      <Text>Test Text that does nothing</Text>
+    <group position={[-2, 1, 1]}>
+    <Root backgroundColor="red" sizeX={3} sizeY={2} flexDirection="row" borderRadius={6}>
+      <Text>This cannot be dragged but clicked</Text>
       <DashVideo src={"https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"} width={400}/>
     </Root>
     </group>
+    <Grabbable position={[2, 1, 1]}>
+    <Root backgroundColor="red" sizeX={3} sizeY={2} flexDirection="row">
+      <Text>This can be dragged but as of now not clicked</Text>
+      <DashVideo src={"https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"} width={400}/>
+    </Root>
+    </Grabbable>
         <NonImmersiveCamera position={[0, 1.5, 4]} />
         <ImmersiveSessionOrigin position={[0, 0, 4]}>
           <Hands type="pointer" />
---
+++ demos/uikit-dash/src/components/apfel/README.md
@@ -0,0 +1,4 @@
+When we use e.g. `npx uikit component add apfel button`, files are created in here to enable us to use the component in our project.
+This is only for the *Apfel* kit.
+
+See [uikit documentation](https://docs.pmnd.rs/uikit/kits/apfel) for more information.

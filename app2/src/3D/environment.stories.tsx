b2b83dbccada4e1724c489ae1e718686538652bb
Finite <33845166+finia2NA@users.noreply.github.com>
2024-06-15 23:51:50 +0200

    Add day and night mode to env

--- app2/src/3D/environment.stories.tsx
+++ app2/src/3D/environment.stories.tsx
@@ -1,9 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
-// import StoryHelper from '../components/StoryHelper';
 import Environment from './environment';
 import { Vector3 } from 'three';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { EffectComposer, Bloom, Noise, Vignette } from '@react-three/postprocessing';
+import { KernelSize } from 'postprocessing';
 
 const meta = {
   title: '3D/Environment',
@@ -13,6 +14,8 @@ const meta = {
     layout: 'centered',
   },
   args: {
+    useOrbitControls: false,
+    nightMode: false,
     position: new Vector3(0, 0, 0),
     scale: new Vector3(1, 1, 1),
     viewDirection: new Vector3(0, 0, 1),
@@ -23,15 +26,31 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const BoxHelper = () => {
+  return (
+    <mesh position={[30, 20, -6]} castShadow receiveShadow>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshBasicMaterial color="red" transparent opacity={1} depthTest={false} />
+    </mesh>)
+}
+
 export const Default: Story = {
-  render: (args) => (
-    <Canvas style={{ height: "100vh", width: "100vw" }}>
-      <hemisphereLight intensity={2} color={0xaaaaff} />
-      <Environment {...args} />
-
-
-      <PerspectiveCamera makeDefault position={[0, 0, 200]} />
-      <OrbitControls />
-    </Canvas>
-  )
+  render: (args) => {
+    return (
+      <Canvas shadows style={{ height: "720px", width: "1440px" }}>
+        <Environment {...args} />
+        <PerspectiveCamera makeDefault position={[25, 5, 25]} ref={(camera) => camera && camera.lookAt(new Vector3(15, 5, 0))} />
+        <gridHelper args={[200, 200]} />
+        <axesHelper args={[100]} />
+        {args.useOrbitControls && <OrbitControls />}
+        <EffectComposer>
+          {/* <DepthOfField focusDistance={30} focalLength={0.02} bokehScale={2} height={480} /> */}
+          <Bloom luminanceThreshold={0.1} luminanceSmoothing={0.9} height={300} kernelSize={KernelSize.HUGE} />
+          <Noise opacity={0.02} />
+          <Vignette eskil={false} offset={0.1} darkness={1.1} />
+        </EffectComposer>
+        {/* <BoxHelper /> */}
+      </Canvas>
+    );
+  }
 };

669fea845de1b884f5e2262b919ee82d806c193e
Finite <33845166+finia2NA@users.noreply.github.com>
2024-06-16 02:55:33 +0200

    Demo a version using props to manage the orbit controls on a higher level in the component tree

--- demos/masking/src/App.js
+++ demos/masking/src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense, useRef, useState, useEffect } from 'react'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { Mask, useGLTF, Bounds, OrbitControls } from '@react-three/drei'
 import * as THREE from 'three'
 
@@ -13,56 +13,43 @@ export function App() {
 }
 
 function Map() {
+  const [handleCameraMove, setHandleCameraMove] = useState(() => () => {});
+
   return (
     <group>
       {/* lights */}
       <directionalLight position={[1, 2, 1.5]} intensity={0.5} castShadow />
       <hemisphereLight intensity={1.5} groundColor="red" />
       <Suspense fallback={null}>
-
-        {/* // start of relevant part
-        // give the mask a unique id and position it in front of the camera */}
         <Mask id={1} position={[0, 0, 2.2]}>
-          {/* define the shape of the mask */}
           <circleGeometry args={[4.0, 128]} />
-
-          {/* define the material of the mask */}
           <meshBasicMaterial
-            // it has a color
             colorWrite={true}
-            // it does not do depth testing -> rendered last
             depthWrite={false}
-            // it's used as a stencil mask
             stencilWrite={true}
             stencilRef={1}
             stencilFunc={THREE.AlwaysStencilFunc}
             stencilFail={THREE.ReplaceStencilOp}
             stencilZFail={THREE.ReplaceStencilOp}
             stencilZPass={THREE.ReplaceStencilOp}
-            // only render the front side, also: it's red
             side={THREE.FrontSide}
             color={new THREE.Color('red')}
           />
         </Mask>
-
-        {/* Bounds is used to tell the camera to fit the content within the given bounds */}
         <Bounds fit clip observe>
-          <Atom scale={8} />
+          <Atom scale={8} setHandleCameraMove={setHandleCameraMove} />
         </Bounds>
       </Suspense>
+      <OrbitControls makeDefault onChange={handleCameraMove} />
     </group>
   )
 }
 
-const Atom = React.memo(({ ...props }) => {
-  // geometry, camera, scene, ref to the mesh
+const Atom = React.memo(({ setHandleCameraMove, ...props }) => {
   const { nodes } = useGLTF('/react-transformed.glb')
-  const { camera, scene } = useThree()
   const ref = useRef()
 
   const handleCameraMove = (event) => {
-    // on camera move, need to update the stencil ref of the mask
-
     ref.current.stencilWrite = true
     ref.current.stencilFunc = THREE.NotEqualStencilFunc
     ref.current.stencilFail = THREE.ReplaceStencilOp
@@ -70,12 +57,15 @@ const Atom = React.memo(({ ...props }) => {
     ref.current.stencilZPass = THREE.ReplaceStencilOp
   }
 
+  useEffect(() => {
+    setHandleCameraMove(() => handleCameraMove)
+  }, [setHandleCameraMove])
+
   return (
     <group>
       <mesh geometry={nodes.atom.geometry} {...props} dispose={null}>
         <meshStandardMaterial ref={ref} color="#33BBFF" />
       </mesh>
-      <OrbitControls makeDefault onChange={handleCameraMove} />
     </group>
   )
 })

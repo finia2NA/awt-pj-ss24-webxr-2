7f037858c5acf17815fc38d459cab831b72c9df2
Finite <33845166+finia2NA@users.noreply.github.com>
2024-06-21 00:20:06 +0200

    Settings Window UI

--- app/src/windows/SettingsWindow.tsx
+++ app/src/windows/SettingsWindow.tsx
@@ -1,8 +1,59 @@
-
+import ButtonGroup from "../components/ButtonGroup";
+import Backdrop from "../components/Backdrop";
+import { Button } from "../components/apfel/button";
+import { Card } from "../components/apfel/card";
+import { Container, Text } from "@react-three/uikit";
+import useColors from "../hooks/useColors";
 
 const SettingsWindow = () => {
+
+  const colors = useColors();
+
+
+  const notImplemented = () => {
+    throw new Error("Function not implemented.");
+  }
+
+
+
+
   return (
-    <></>
+    <Card
+      alignSelf={"flex-start"}
+      paddingTop={24}
+      paddingBottom={40}
+      paddingX={20}
+      flexDirection={"column"}
+      gap={16}
+    >
+
+      <Text fontSize={32}>XR-DVBI</Text>
+
+      <Container flexDirection={"column"} gap={1}>
+
+        <ButtonGroup roundTop title="Theme" options={["System", "Light", "Dark"]} selected={0} onClick={notImplemented} />
+
+        <ButtonGroup title="DVBI-API" options={["Default", "Custom"]} selected={0} onClick={notImplemented} />
+
+        <Backdrop
+          borderBottomRadius={20}
+          justifyContent={"center"}
+          gap={6}
+        >
+          <Text fontSize={12} fontWeight={'light'}>2024 by Group 2. MIT License. See the</Text>
+
+          <Button variant="pill" platter size={36}
+            backgroundColor={colors.background} paddingX={14}>
+            <Text fontSize={12} fontWeight={"light"}>
+              Projects we are using
+            </Text>
+          </Button>
+
+        </Backdrop>
+
+      </Container>
+
+    </Card>
   );
 }
 

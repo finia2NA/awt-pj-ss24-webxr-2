dad4d893bbc9ffb9824d7974a6d773d2753f22ce
Guchen <GuchenGit@users.noreply.github.com>
2024-06-27 10:54:08 +0200

    Home recommendation component (and HeartButton fixes as well as additions) (#35)
    * Added HomeRecommendation
    
    * Fixed missing text color
    
    * Fixed HeartButton double clicking. Added heart button to ChannelListElement.
    
    * Added documentation for new things

--- app/src/components/ChannelList/ChannelListElement.tsx
+++ app/src/components/ChannelList/ChannelListElement.tsx
@@ -1,13 +1,32 @@
 import { Container, Image, Text } from "@react-three/uikit";
 import useColors from "../../hooks/useColors";
 import Backdrop from "../Backdrop";
+import HeartButton from "./HeartButton";
 
 export interface ChannelListElementProps {
+    /**
+     * Global channel ID (e.g. 1)
+     */
     number: number;
+    /**
+     * Name of the channel
+     */
     name: string;
+    /**
+     * Description (most likely show title)
+     */
     description: string;
+    /**
+     * Time at which the show starts (e.g. 10:00)
+     */
     timeStart: string;
+    /**
+     * Time at which the show ends (e.g. 11:00)
+     */
     timeEnd: string;
+    /**
+     * URL of the image to display
+     */
     imageUrl: string;
 }
 
@@ -15,18 +34,21 @@ export interface ChannelListElementProps {
  * Renders a single channel inside the list of channels.
  */
 const ChannelListElement = ({ number, name, description, timeStart, timeEnd, imageUrl }: ChannelListElementProps) => {
-    const colors = useColors()
+    const colors = useColors();
 
     return (
         <Backdrop height={90} paddingLeft={0} paddingRight={0} paddingY={0} gap={0} borderRadius={30} width={240} margin={0} marginTop={0}>
             <Container width={30} paddingLeft={10} borderRightWidth={2} borderColor={colors.foreground} display={"flex"} flexDirection={"column"} justifyContent={"space-evenly"} height={90} >
-                <Text>{number.toString()}</Text>
+                <Text color={colors.foreground}>{number.toString()}</Text>
                 <Image width={20} height={20} src={imageUrl}></Image>
             </Container>
             <Container paddingLeft={8} display={"flex"} flexDirection={"column"} justifyContent={"space-evenly"} width={150}>
-                <Text paddingBottom={10}>{name}</Text>
-                <Text>{description}</Text>
-                <Text>{timeStart + " - " + timeEnd}</Text>
+                <Text color={colors.foreground} paddingBottom={10}>{name}</Text>
+                <Text color={colors.foreground}>{description}</Text>
+                <Text color={colors.foreground}>{timeStart + " - " + timeEnd}</Text>
+            </Container>
+            <Container height={90} display={"flex"} justifyContent={"flex-start"} flexDirection={"column"} alignItems={"flex-start"}>
+                <HeartButton channelID={number.toString()}></HeartButton>
             </Container>
         </Backdrop>
     )
---
+++ app/src/components/Home/HomeRecommendation.tsx
@@ -0,0 +1,48 @@
+import { Container, Image, Text } from "@react-three/uikit";
+import useColors from "../../hooks/useColors";
+import Backdrop from "../Backdrop";
+
+export interface HomeRecommendationProps {
+    /**
+     * Name of the channel
+     */
+    name: string;
+    /**
+     * Description (most likely show title)
+     */
+    description: string;
+    /**
+     * Time at which the show starts (e.g. 10:00)
+     */
+    timeStart: string;
+    /**
+     * Time at which the show ends (e.g. 11:00)
+     */
+    timeEnd: string;
+    /**
+     * URL of the image to display
+     */
+    imageUrl: string;
+}
+
+/**
+ * Renders a home recommendation component.
+ */
+const HomeRecommendation = ({ name, description, timeStart, timeEnd, imageUrl }: HomeRecommendationProps) => {
+    const colors = useColors();
+
+    return (
+        <Backdrop height={75} paddingLeft={0} paddingRight={0} paddingY={0} gap={0} borderRadius={20} width={240} margin={0} marginTop={0}>
+            <Container width={55} paddingLeft={10} display={"flex"} flexDirection={"column"} justifyContent={"space-evenly"} height={90} >
+                <Image width={45} src={imageUrl}></Image>
+            </Container>
+            <Container width={140} paddingLeft={8} display={"flex"} flexDirection={"column"} justifyContent={"space-evenly"}>
+                <Text color={colors.foreground} paddingBottom={10}>{name}</Text>
+                <Text color={colors.foreground}>{description}</Text>
+                <Text color={colors.foreground}>{timeStart + " - " + timeEnd}</Text>
+            </Container>
+        </Backdrop>
+    );
+}
+
+export default HomeRecommendation;

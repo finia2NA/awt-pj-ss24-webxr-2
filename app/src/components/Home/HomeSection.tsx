f1879d385739545045894f72fcaffe4df572653e
Guchen <GuchenGit@users.noreply.github.com>
2024-06-27 12:02:33 +0200

    Added very basic HomeSection (#36)

---
+++ app/src/components/Home/HomeSection.tsx
@@ -0,0 +1,27 @@
+import { Container, Text } from "@react-three/uikit";
+import HomeRecommendation, { HomeRecommendationProps } from "./HomeRecommendation";
+import useColors from "../../hooks/useColors";
+
+export interface HomeSectionProps {
+    /**
+     * Title of the section
+     */
+    title: string;
+    channels: HomeRecommendationProps[];
+}
+
+const HomeSection = ({ title, channels }: HomeSectionProps) => {
+    const colors = useColors();
+    return (
+        <Container width={2000} display={"flex"} flexDirection={"column"} gapRow={8} >
+            <Text color={colors.foreground} fontSize={13} fontWeight={"medium"}>{title}</Text>
+            <Container display={"flex"} gap={10} flexDirection={"row"} overflow={"scroll"} height={100}>
+                {channels.map((channel, index) => (
+                    <HomeRecommendation key={index} {...channel} />
+                ))}
+            </Container>
+        </Container>
+    );
+}
+
+export default HomeSection;

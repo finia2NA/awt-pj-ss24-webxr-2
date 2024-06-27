b67b4e99dba79981b44e32bda4f87fd6af8474b4
Guchen <GuchenGit@users.noreply.github.com>
2024-06-27 12:02:33 +0200

    Added very basic HomeSection (#36)

--- app/src/components/Home/HomeRecommendation.tsx
+++ app/src/components/Home/HomeRecommendation.tsx
@@ -32,7 +32,7 @@ const HomeRecommendation = ({ name, description, timeStart, timeEnd, imageUrl }:
     const colors = useColors();
 
     return (
-        <Backdrop height={75} paddingLeft={0} paddingRight={0} paddingY={0} gap={0} borderRadius={20} width={240} margin={0} marginTop={0}>
+        <Backdrop height={75} paddingLeft={0} paddingRight={0} paddingY={0} gap={0} borderRadius={20} width={240} margin={0} marginTop={0} flexShrink={0} flexGrow={0}>
             <Container width={55} paddingLeft={10} display={"flex"} flexDirection={"column"} justifyContent={"space-evenly"} height={90} >
                 <Image width={45} src={imageUrl}></Image>
             </Container>
---
+++ app/src/components/Home/HomeSection.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import HomeSection from './HomeSection';
+import StoryHelper from '../../StoryHelper';
+
+const meta: Meta<typeof HomeSection> = {
+    title: 'Components/Home/HomeSection',
+    component: HomeSection,
+    tags: ["autodocs"],
+    parameters: {
+        layout: 'centered',
+    },
+    args: {
+        title: "Your Favorite Channels",
+        channels: [
+            {
+                name: "TestName",
+                description: "TestDescription",
+                timeStart: "10:00",
+                timeEnd: "11:00",
+                //imageUrl: "https://itv-api.ard.de/ardstart/img/services/28106.png",
+                imageUrl: "https://corsproxy.io/?https%3A%2F%2Fitv-api.ard.de%2Fardstart%2Fimg%2Fservices%2F28106.png"
+            },
+            {
+                name: "TestName",
+                description: "TestDescription",
+                timeStart: "10:00",
+                timeEnd: "11:00",
+                //imageUrl: "https://itv-api.ard.de/ardstart/img/services/28106.png",
+                imageUrl: "https://corsproxy.io/?https%3A%2F%2Fitv-api.ard.de%2Fardstart%2Fimg%2Fservices%2F28106.png"
+            },
+            {
+                name: "TestName",
+                description: "TestDescription",
+                timeStart: "10:00",
+                timeEnd: "11:00",
+                //imageUrl: "https://itv-api.ard.de/ardstart/img/services/28106.png",
+                imageUrl: "https://corsproxy.io/?https%3A%2F%2Fitv-api.ard.de%2Fardstart%2Fimg%2Fservices%2F28106.png"
+            },
+            {
+                name: "TestName",
+                description: "TestDescription",
+                timeStart: "10:00",
+                timeEnd: "11:00",
+                //imageUrl: "https://itv-api.ard.de/ardstart/img/services/28106.png",
+                imageUrl: "https://corsproxy.io/?https%3A%2F%2Fitv-api.ard.de%2Fardstart%2Fimg%2Fservices%2F28106.png"
+            }
+        ]
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    render: (args) => {
+        return (
+            <>
+                <StoryHelper>
+                    <HomeSection {...args} />
+                </StoryHelper>
+            </>
+        )
+    }
+};

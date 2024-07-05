05f2ba04c292428f312000913cd586140e997656
Guchen <GuchenGit@users.noreply.github.com>
2024-07-05 12:33:07 +0200

    Added date selection to Guide, made loading and error screen better (#50)
    * Added date selection, improved loading screen
    
    * Update guide with error, defaultDate (so no going to yesterday). Starts at 00:00 on future days

--- app/src/components/Guide/GuideWindow.tsx
+++ app/src/components/Guide/GuideWindow.tsx
@@ -4,39 +4,77 @@ import Guide, { GuideProps } from "./Guide";
 import Dropdown from "../ChannelList/Dropdown";
 import { useState } from "react";
 import { Button } from "../apfel/button";
+import { alterDateDays, getDateISO } from "../../utils/dateHelpers";
 
 export interface GuideWindowProps extends Omit<GuideProps, 'zoomLevel'> {
     time: string;
     regions: string[];
     width?: number;
     defaultZoomLevel: number;
+    date: Date;
+    setDate?: (date: Date) => void;
+    loading?: boolean;
+    error?: boolean;
+    defaultDate?: Date;
 }
-const GuideWindow = ({ time, regions, width, defaultZoomLevel, ...rest }: GuideWindowProps) => {
+const GuideWindow = ({ time, regions, width, defaultZoomLevel, date, setDate = () => { }, loading = false, error = false, defaultDate, overrideStartTime, ...rest }: GuideWindowProps) => {
     const [activeIndex, setActiveIndex] = useState(0);
     const [zoomLevel, setZoomLevel] = useState(defaultZoomLevel);
 
-    return (
-        <Card width={width ? width : "auto"}>
-            <Container display={"flex"} flexDirection={"column"} gap={10} paddingX={20} paddingY={20} padding={20}>
-                <Container display={"flex"} flexDirection={"row"} justifyContent={"space-between"} >
-                    <Container display={"flex"} flexDirection={"row"} gap={10}>
-                        <Text fontWeight={"bold"} fontSize={20}>TV Guide for: </Text>
-                        <Dropdown activeIndex={activeIndex} items={regions} onSelectItem={(index) => setActiveIndex(index)} />
-                        <Container display={"flex"} flexDirection={"row"} alignItems={"baseline"}>
-                            <Button onClick={() => setZoomLevel(zoomLevel - 0.1 > 0 ? zoomLevel - 0.1 : 0)}>
-                                <Text fontSize={20} fontWeight={"bold"}>-</Text>
-                            </Button>
-                            <Text>Zoom: {Math.floor(zoomLevel * 100).toString()}%</Text>
-                            <Button onClick={() => setZoomLevel(zoomLevel + 0.1 < 10 ? zoomLevel + 0.1 : 10)}>
-                                <Text fontSize={20} fontWeight={"bold"}>+</Text>
-                            </Button>
-                        </Container>
-                    </Container>
-                    <Text>{time}</Text>
+    const guideDisplay = () => {
+        if(loading) {
+            return (
+                <Container display={"flex"} justifyContent={"center"} alignItems={"center"} height={300} width={900}>
+                    <Text fontSize={20}>Loading...</Text>
                 </Container>
-                <Guide zoomLevel={zoomLevel} {...rest} />
-            </Container>
-        </Card>
+            )
+        } else if (error) {
+            return (
+                <Container display={"flex"} justifyContent={"center"} alignItems={"center"} height={300} width={900}>
+                    <Text fontSize={20}>Error</Text>
+                </Container>
+            )
+        } else {
+            return <Guide zoomLevel={zoomLevel} overrideStartTime={defaultDate?.getTime() === date.getTime() ? overrideStartTime : "00:00"} {...rest} />
+        }
+    }
+
+    const buttonsDisabled = () => {
+        const loadingOrError = loading || error;
+        return loadingOrError;
+    }
+
+    const dateReached = defaultDate ? date <= defaultDate : false;
+
+    return(
+        <Card width = { width? width: "auto" } >
+                <Container display={"flex"} flexDirection={"column"} gap={10} paddingX={20} paddingY={20} padding={20}>
+                    <Container display={"flex"} flexDirection={"row"} justifyContent={"space-between"} >
+                        <Container display={"flex"} flexDirection={"row"} gap={10}>
+                            <Text fontWeight={"bold"} fontSize={20}>TV Guide for: </Text>
+                            <Dropdown activeIndex={activeIndex} items={regions} onSelectItem={(index) => setActiveIndex(index)} />
+                            <Container display={"flex"} flexDirection={"row"} alignItems={"baseline"}>
+                                <Button onClick={() => { !buttonsDisabled() ? setZoomLevel(zoomLevel - 0.1 > 0 ? zoomLevel - 0.1 : 0) : console.log("disabled") }} disabled={buttonsDisabled()}>
+                                    <Text fontSize={20} fontWeight={"bold"}>-</Text>
+                                </Button>
+                                <Text>Zoom: {Math.floor(zoomLevel * 100).toString()}%</Text>
+                                <Button onClick={() => { !buttonsDisabled() ? setZoomLevel(zoomLevel + 0.1 < 10 ? zoomLevel + 0.1 : 10) : console.log("disabled") }} disabled={buttonsDisabled()}>
+                                    <Text fontSize={20} fontWeight={"bold"}>+</Text>
+                                </Button>
+                                <Button onClick={() => { (!buttonsDisabled() && !dateReached) ? setDate(alterDateDays(date, -1)) : console.log("disabled") }} disabled={!(!buttonsDisabled() && !dateReached)}>
+                                    <Text fontSize={20} fontWeight={"bold"}>-</Text>
+                                </Button>
+                                <Text>{getDateISO(date)}</Text>
+                                <Button onClick={() => { !buttonsDisabled() ? setDate(alterDateDays(date, 1)) : console.log("disabled") }} disabled={buttonsDisabled()}>
+                                    <Text fontSize={20} fontWeight={"bold"}>+</Text>
+                                </Button>
+                            </Container>
+                        </Container>
+                        <Text>{time}</Text>
+                    </Container>
+                    {guideDisplay()}
+                </Container>
+        </Card >
     )
 }
 

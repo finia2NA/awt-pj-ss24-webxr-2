f65aaba130e6e11c8adab74153d1ebd74c73084c
Guchen <GuchenGit@users.noreply.github.com>
2024-07-05 08:53:22 +0200

    Added active channel to guide. Able to switch channels from there (#48)

--- app/src/components/Guide/GuideStrip.tsx
+++ app/src/components/Guide/GuideStrip.tsx
@@ -3,14 +3,16 @@ import GuideStripProgram, { GuideStripProgramProps } from "./GuideStripProgram";
 
 export interface GuideStripProps {
     programs: GuideStripProgramProps[];
+    active?: boolean;
+    handleClick?: () => void;
 }
 
-const GuideStrip = ({ programs }: GuideStripProps) => {
+const GuideStrip = ({ programs, active, handleClick = () => {} }: GuideStripProps) => {
 
     return (
         <Container display={"flex"} flexDirection={"row"} alignItems={"center"} gap={0} justifyContent={"flex-start"} height={65}>
             {programs.map((program, index) => (
-                <GuideStripProgram key={index} {...program} />
+                <GuideStripProgram key={index} active={active} {...program} handleClick={handleClick} />
             ))}
         </Container>
     )

2fe3e9a45a759213ed37897d282b55fdbb1bc179
Finite <33845166+finia2NA@users.noreply.github.com>
2024-07-09 22:09:38 +0200

    Chevron Buttons in Guide Window

--- app/src/components/Guide/GuideWindow.tsx
+++ app/src/components/Guide/GuideWindow.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Button } from "../apfel/button";
 import { alterDateDays, getDateISO } from "../../utils/dateHelpers";
 import useColors from "../../hooks/useColors";
+import { ChevronLeftIcon, ChevronRightIcon } from "@react-three/uikit-lucide";
 
 export interface GuideWindowProps extends Omit<GuideProps, 'zoomLevel'> {
     time: string;
@@ -24,7 +25,7 @@ const GuideWindow = ({ time, regions, width, defaultZoomLevel, date, setDate = (
     const colors = useColors();
 
     const guideDisplay = () => {
-        if(loading) {
+        if (loading) {
             return (
                 <Container display={"flex"} justifyContent={"center"} alignItems={"center"} height={300} width={900}>
                     <Text fontSize={30} fontWeight={"medium"} color={colors.primary} >Loading...</Text>
@@ -48,34 +49,34 @@ const GuideWindow = ({ time, regions, width, defaultZoomLevel, date, setDate = (
 
     const dateReached = defaultDate ? date <= defaultDate : false;
 
-    return(
-        <Card width = { width? width: "auto" } >
-                <Container display={"flex"} flexDirection={"column"} gap={10} paddingX={20} paddingY={20} padding={20}>
-                    <Container display={"flex"} flexDirection={"row"} justifyContent={"space-between"} >
-                        <Container display={"flex"} flexDirection={"row"} gap={10}>
-                            <Text fontWeight={"bold"} fontSize={20}>TV Guide for: </Text>
-                            <Dropdown activeIndex={activeIndex} items={regions} onSelectItem={(index) => setActiveIndex(index)} />
-                            <Container display={"flex"} flexDirection={"row"} alignItems={"baseline"}>
-                                <Button onClick={() => { !buttonsDisabled() ? setZoomLevel(zoomLevel - 0.1 > 0 ? zoomLevel - 0.1 : 0) : console.log("disabled") }} disabled={buttonsDisabled()}>
-                                    <Text fontSize={20} fontWeight={"bold"}>-</Text>
-                                </Button>
-                                <Text>Zoom: {Math.floor(zoomLevel * 100).toString()}%</Text>
-                                <Button onClick={() => { !buttonsDisabled() ? setZoomLevel(zoomLevel + 0.1 < 10 ? zoomLevel + 0.1 : 10) : console.log("disabled") }} disabled={buttonsDisabled()}>
-                                    <Text fontSize={20} fontWeight={"bold"}>+</Text>
-                                </Button>
-                                <Button onClick={() => { (!buttonsDisabled() && !dateReached) ? setDate(alterDateDays(date, -1)) : console.log("disabled") }} disabled={!(!buttonsDisabled() && !dateReached)}>
-                                    <Text fontSize={20} fontWeight={"bold"}>-</Text>
-                                </Button>
-                                <Text>{getDateISO(date)}</Text>
-                                <Button onClick={() => { !buttonsDisabled() ? setDate(alterDateDays(date, 1)) : console.log("disabled") }} disabled={buttonsDisabled()}>
-                                    <Text fontSize={20} fontWeight={"bold"}>+</Text>
-                                </Button>
-                            </Container>
+    return (
+        <Card width={width ? width : "auto"} >
+            <Container display={"flex"} flexDirection={"column"} gap={10} paddingX={20} paddingY={20} padding={20}>
+                <Container display={"flex"} flexDirection={"row"} justifyContent={"space-between"} >
+                    <Container display={"flex"} flexDirection={"row"} gap={10}>
+                        <Text fontWeight={"bold"} fontSize={20}>TV Guide for: </Text>
+                        <Dropdown activeIndex={activeIndex} items={regions} onSelectItem={(index) => setActiveIndex(index)} />
+                        <Container display={"flex"} flexDirection={"row"} alignItems={"baseline"}>
+                            <Button onClick={() => { !buttonsDisabled() ? setZoomLevel(zoomLevel - 0.1 > 0 ? zoomLevel - 0.1 : 0) : console.log("disabled") }} disabled={buttonsDisabled()}>
+                                <Text fontSize={20} fontWeight={"bold"}>-</Text>
+                            </Button>
+                            <Text>Zoom: {Math.floor(zoomLevel * 100).toString()}%</Text>
+                            <Button onClick={() => { !buttonsDisabled() ? setZoomLevel(zoomLevel + 0.1 < 10 ? zoomLevel + 0.1 : 10) : console.log("disabled") }} disabled={buttonsDisabled()}>
+                                <Text fontSize={20} fontWeight={"bold"}>+</Text>
+                            </Button>
+                            <Button onClick={() => { (!buttonsDisabled() && !dateReached) ? setDate(alterDateDays(date, -1)) : console.log("disabled") }} disabled={!(!buttonsDisabled() && !dateReached)}>
+                                <ChevronLeftIcon />
+                            </Button>
+                            <Text>{getDateISO(date)}</Text>
+                            <Button onClick={() => { !buttonsDisabled() ? setDate(alterDateDays(date, 1)) : console.log("disabled") }} disabled={buttonsDisabled()}>
+                                <ChevronRightIcon />
+                            </Button>
                         </Container>
-                        <Text>{time}</Text>
                     </Container>
-                    {guideDisplay()}
+                    <Text>{time}</Text>
                 </Container>
+                {guideDisplay()}
+            </Container>
         </Card >
     )
 }

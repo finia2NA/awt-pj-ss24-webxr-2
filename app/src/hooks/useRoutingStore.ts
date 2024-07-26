d22eff888a10d20b63b0a759b6690fce8f63e092
Finite <33845166+finia2NA@users.noreply.github.com>
2024-07-27 00:51:49 +0200

    Add persistence to tuned channel

--- app/src/hooks/useRoutingStore.ts
+++ app/src/hooks/useRoutingStore.ts
@@ -1,7 +1,15 @@
 /**
- * This file contains a hook for doing the global routing and cannel selection in the app.
+ * This file contains hooks for doing the global routing and cannel selection in the app.
  * 
- * The architecture is as follows:
+ * The tuned channel is persisted, but the route is not.
+ * Thus, in reality there are 3 hooks here:
+ * - useRoutingPartStore: simple, non-persisted routing store
+ * - useChannelPartStore: persisted channel store
+ * - useRoutingStore: combination of the above two
+ * 
+ * -----------------------------------------------------
+ * 
+ * The usage and architecture of this is as follows:
  * A lot of components have the oportunity to change the currently selected screen and tuned channel
  * (EG guide, channel list, channel number, home, tab bar.)
  * 
@@ -12,6 +20,7 @@
 
 /* eslint-disable no-unused-vars */
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 /**
  * Enum representing the different routes in the application.
@@ -23,31 +32,49 @@ export enum Route {
   SETTINGS = "SETTINGS",
 }
 
-/**
- * Interface for the routing store, defining the state and actions.
- */
-export interface RoutingStore {
+interface RoutingPartStore {
   route: Route; // The current route of the application.
   setRoute: (route: Route) => void; // Function to set a new route.
+}
 
+interface ChannelPartStore {
   tunedChannel: string | null; // The currently tuned channel, or null if none.
   setTunedChannel: (channel: string) => void; // Function to set the tuned channel.
 }
 
+export interface RoutingStore extends RoutingPartStore, ChannelPartStore { }
+
 /**
  * Hook to use the routing store, which manages the application's routing state.
  */
-const useRoutingStore = create<RoutingStore>((set) => {
-  // TODO: get this from the API, not hardcoded
-  const firstService = "tag:mitxp.com,2021:1.1019.10301";
+const useRoutingPartStore = create<RoutingPartStore>((set) => ({
+  route: Route.HOME, // Initialize the route to the HOME route.
+  setRoute: (route: Route) => set({ route }), // Update the route in the store.
+}));
+
+const useChannelPartStore = create<ChannelPartStore>()(
+  persist(
+    (set) => ({
+      tunedChannel: "tag:zdf.de,2020:zdf",
+      setTunedChannel: (channel: string) => set({ tunedChannel: channel }),
+    }),
+    {
+      name: 'tuned-channel-storage',
+      storage: createJSONStorage(() => localStorage)
+    }
+  )
+)
+
+const useRoutingStore = () => {
+  const { route, setRoute } = useRoutingPartStore(state => state);
+  const { tunedChannel, setTunedChannel } = useChannelPartStore(state => state);
 
   return {
-    route: Route.HOME, // Initialize the route to the HOME route.
-    setRoute: (route: Route) => set({ route }), // Update the route in the store.
-
-    tunedChannel: firstService ? firstService : null, // Initialize the tuned channel.
-    setTunedChannel: (channel: string) => set({ tunedChannel: channel }), // Update the tuned channel in the store.
+    route,
+    setRoute,
+    tunedChannel,
+    setTunedChannel
   };
-});
+}
 
 export default useRoutingStore;

9ac9414217e8cc0b99b298b48f9573c8a04ce10b
Finite <33845166+finia2NA@users.noreply.github.com>
2024-07-27 00:53:40 +0200

    add docsssss

--- app/src/hooks/useRoutingStore.ts
+++ app/src/hooks/useRoutingStore.ts
@@ -32,39 +32,56 @@ export enum Route {
   SETTINGS = "SETTINGS",
 }
 
+/**
+ * Interface for the routing part of the store.
+ */
 interface RoutingPartStore {
   route: Route; // The current route of the application.
   setRoute: (route: Route) => void; // Function to set a new route.
 }
 
+/**
+ * Interface for the channel part of the store.
+ */
 interface ChannelPartStore {
   tunedChannel: string | null; // The currently tuned channel, or null if none.
   setTunedChannel: (channel: string) => void; // Function to set the tuned channel.
 }
 
+/**
+ * Interface for the combined routing and channel store.
+ */
 export interface RoutingStore extends RoutingPartStore, ChannelPartStore { }
 
 /**
- * Hook to use the routing store, which manages the application's routing state.
+ * Hook to use the routing part store
  */
 const useRoutingPartStore = create<RoutingPartStore>((set) => ({
   route: Route.HOME, // Initialize the route to the HOME route.
   setRoute: (route: Route) => set({ route }), // Update the route in the store.
 }));
 
+/**
+ * Hook to use the channel part store, which manages the application's tuned channel state.
+ * The tuned channel is persisted across sessions using localStorage.
+ */
 const useChannelPartStore = create<ChannelPartStore>()(
   persist(
     (set) => ({
-      tunedChannel: "tag:zdf.de,2020:zdf",
-      setTunedChannel: (channel: string) => set({ tunedChannel: channel }),
+      tunedChannel: "tag:zdf.de,2020:zdf", // Default tuned channel.
+      setTunedChannel: (channel: string) => set({ tunedChannel: channel }), // Update the tuned channel in the store.
     }),
     {
-      name: 'tuned-channel-storage',
-      storage: createJSONStorage(() => localStorage)
+      name: 'tuned-channel-storage', // Key for the localStorage item.
+      storage: createJSONStorage(() => localStorage), // Use localStorage for persistence.
     }
   )
 )
 
+/**
+ * Hook to use the combined routing and channel store.
+ * This combines both the routing state and the tuned channel state into a single store.
+ */
 const useRoutingStore = () => {
   const { route, setRoute } = useRoutingPartStore(state => state);
   const { tunedChannel, setTunedChannel } = useChannelPartStore(state => state);

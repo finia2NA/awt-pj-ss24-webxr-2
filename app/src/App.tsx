0c766586ac68baf435c8728c55f72fa8ecc28c4b
Guchen <GuchenGit@users.noreply.github.com>
2024-08-08 12:30:28 +0200

    Added secondary movement mode that is controller based, switching between the two possible in settings (#80)
    * Fixed pointer events
    
    * New method for moving windows, not fully working (distance)
    
    * Added new (non-perfect) movement
    
    * Add switching between the two window movement modes, more documentation

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,12 @@
-import { DefaultXRController, XR, XROrigin, createXRStore } from '@react-three/xr';
-const rayOptions = {rayPointer: {cursorModel: {color: "blue", opacity: 1, size: 0.2}, rayModel: {opacity: 0.7, maxLength: 5}}};
-const store = createXRStore({ controller: rayOptions, hand: rayOptions});
+import { XR, XROrigin, createXRStore, useXRControllerState } from '@react-three/xr';
+const rayOptions = { rayPointer: { cursorModel: { color: "blue", opacity: 1, size: 0.2 }, rayModel: { opacity: 0.7, maxLength: 5 } } };
+const store = createXRStore({ controller: rayOptions, hand: rayOptions });
 
-import { isXIntersection } from "@coconut-xr/xinteraction";
-
-import { useState, useRef, useEffect } from 'react';
-import { Canvas, ThreeEvent } from "@react-three/fiber";
+import { useState, useRef } from 'react';
+import { Canvas, ThreeEvent, useFrame } from "@react-three/fiber";
 import { Root, Container, ComponentInternals } from "@react-three/uikit";
 
-import { Vector3 } from "three";
+import { Group as ThreeGroup, Vector3 } from "three";
 
 import Tabs from "./components/Tabs";
 import BottomBar from "./windows/BottomBar";
@@ -22,21 +20,21 @@ import useRoutingStore, { Route } from './hooks/useRoutingStore';
 
 import KeyboardUI from "./components/KeyboardUI";
 import useKeyboardStore from './hooks/useKeyboardStore.ts';
-import useSettingsStore, { BiTheme, SettingsState } from "./hooks/useSettingsStore.ts";
+import useSettingsStore, { BiTheme, MovementMode, SettingsState } from "./hooks/useSettingsStore.ts";
 
 
-
-const sessionOptions = {
-  requiredFeatures: ["local-floor", "hand-tracking"]
-};
-
+/**
+ * The main application component.
+ * It contains the main structure of the application and handles the routing between different views.
+ * Basically the entry point of the application that contains everything.
+ */
 export default function App() {
   const cameraDistance = -3;
 
   const [immersionLevel, setImmersionLevel] = useState(0);
 
   const { route, setRoute } = useRoutingStore();
-  const { biTheme } = useSettingsStore((state) => state) as SettingsState;
+  const { biTheme, movementMode } = useSettingsStore((state) => state) as SettingsState;
 
   const handleTabSelection = (route: Route) => {
     setRoute(route);
@@ -54,23 +52,15 @@ export default function App() {
     position: Vector3;
     rotation: Vector3;
   }>();
+  const groupRef = useRef<ThreeGroup>();
+  const moveDistanceOffset = useRef(new Vector3(0, 0, 0));
 
-  const [pixelRatio, setPixelRatio] = useState(window.devicePixelRatio * 1.2);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setPixelRatio(window.devicePixelRatio * 1.2);
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-  
-
+  /**
+   * Setup everything upon pointer down event.
+   * This includes capturing the pointer, setting the initial position and rotation of the view to be used in pointer move.
+   */
   const handlePointerDown = (e: ThreeEvent<PointerEvent>) => {
-    console.log("Pointer down")
     if (
       view.current != null &&
       downState.current == null
@@ -105,8 +95,12 @@ export default function App() {
       return;
     }
     downState.current = undefined;
+    moveDistanceOffset.current.set(0, 0, 0);
   };
 
+  /**
+   * Handles the pointer move event, so that the view can be moved around.
+   */
   const handlePointerMove = (e: ThreeEvent<PointerEvent>) => {
     if (
       handle.current == null ||
@@ -116,25 +110,68 @@ export default function App() {
     ) {
       return;
     }
+    // Decide which movement mode to use from the settings
+    if (movementMode === MovementMode.CURSOR_BASED) {
+      const scale = 90; // Adjust this value as needed
 
-    const scale = 90; // Adjust this value as needed
+      let delta = e.point.sub(downState.current.point.clone());
+      let scaledDelta = new Vector3(delta.x * scale, -delta.y * scale, delta.z * scale);
+      let newPosition = downState.current.position.clone().add(scaledDelta);
 
-    let delta = e.point.sub(downState.current.point.clone());
-    let scaledDelta = new Vector3(delta.x * scale, -delta.y * scale, delta.z * scale);
-    let newPosition = downState.current.position.clone().add(scaledDelta);
+      const disCamera = Math.abs(cameraDistance);
+      const scaleRot = 20;
+      let rotY = downState.current.rotation.y - (delta.x * scaleRot / disCamera);
+      let rotX = downState.current.rotation.x + (delta.y * scaleRot / disCamera);
 
-    const disCamera = Math.abs(cameraDistance);
-    const scaleRot = 20;
-    let rotY = downState.current.rotation.y - (delta.x * scaleRot / disCamera);
-    let rotX = downState.current.rotation.x + (delta.y * scaleRot / disCamera);
+      view.current.setStyle({
+        ...view.current.getStyle(),  // Preserve other styles
+        ...{
+          transformTranslateX: newPosition.x, transformTranslateY: newPosition.y,
+          transformRotateX: rotX, transformRotateY: rotY
+        }
+      });
+    } else if (movementMode === MovementMode.CONTROLLER_BASED && groupRef.current) {
+      groupRef.current.position.copy(e.point.add(moveDistanceOffset.current));
+      groupRef.current.setRotationFromQuaternion(e.pointer.intersection.pointerQuaternion);
+    }
+  };
 
-    view.current.setStyle({
-      ...view.current.getStyle(),  // Preserve other styles
-      ...{
-        transformTranslateX: newPosition.x, transformTranslateY: newPosition.y,
-        transformRotateX: rotX, transformRotateY: rotY
+  /**
+   * Component that returns the XROrigin but enables using the controller in various ways.
+   * Currently only used for moving the window closer or further away when moving it around
+   * with the controller.
+   */
+  const Locomotion = (props: any) => {
+    // Get the controller state for the right controller
+    const controller = useXRControllerState('right');
+    // If we want to reference the XROrigin later
+    const ref = useRef<ThreeGroup>(null);
+
+    // Called every frame, including delta
+    // Mainly useful for controller input because of that
+    useFrame((_, delta) => {
+      // Make sure everything is available
+      if (ref.current == null || controller == null) {
+        return
       }
-    });
+      // Get the thumbstick state from the controller
+      const thumstickState = controller.gamepad['xr-standard-thumbstick']
+      if (thumstickState == null) {
+        return
+      }
+      if (!downState.current || !groupRef.current) {
+        return;
+      }
+      // Calculate the amount to move based on the thumbstick state
+      const moveAmount = (thumstickState.yAxis ?? 0) * delta * 10;
+      const direction = new Vector3();
+      // Make sure we're moving in the right direction
+      // FIXME: This can be problematic and the vector often doesn't align perfectly.
+      controller.object?.getWorldDirection(direction);
+      direction.multiplyScalar(moveAmount);
+      moveDistanceOffset.current.add(direction);
+    })
+    return <XROrigin ref={ref} {...props} />
   };
 
   return (
@@ -148,76 +185,76 @@ export default function App() {
     >
       <button onClick={() => store.enterAR()}>Enter AR</button>
       <button onClick={() => store.enterVR()}>Enter VR</button>
-      <Canvas >
-      <XR store={store}>
-        {/* <OrbitControls /> */}
-        <group position={[0, 2, cameraDistance]}>
-          <Root
-            ref={view}
-            sizeX={20} sizeY={3}
-            transformTranslateX={route === Route.TV ? 210 : 0}
-            flexDirection="column"
-            borderRadius={6}
-            pixelSize={0.008}
-          >
-            <Container
-              flexDirection="row"
-              height={"auto"}
-              alignSelf={"center"}
+      {/* localClippingEnabled is required for images to be able to be cut off, 
+      e.g. when being partially visible while scrolling*/}
+      <Canvas gl={{ localClippingEnabled: true }}> 
+        <XR store={store}>
+          {/* <OrbitControls /> */}
+          <group position={[0, 2, cameraDistance]} ref={groupRef}>
+            <Root
+              ref={view}
+              sizeX={20} sizeY={3}
+              transformTranslateX={route === Route.TV ? 210 : 0}
+              flexDirection="column"
+              borderRadius={6}
+              pixelSize={0.008}
             >
               <Container
-                marginRight={50}
+                flexDirection="row"
+                height={"auto"}
                 alignSelf={"center"}
-                ref={tabs}
-                marginTop={keyboardVisible ? -223 : 0}
               >
-                <Tabs setSelectedRoute={handleTabSelection} selectedRoute={route} />
-              </Container>
-              <Container flexDirection={"column"} height={"auto"} alignItems={"center"}>
-                <Container height={"auto"}>
-                  {route === "HOME" && <Home />}
-                  {route === "TV" && <Tv viewRef={view} handleRef={bar} tabsRef={tabs} />}
-                  {route === "GUIDE" && <GuideView viewRef={view} handleRef={handle} tabsRef={tabs} />}
-                  {route === "SETTINGS" && <SettingsView />}
+                <Container
+                  marginRight={50}
+                  alignSelf={"center"}
+                  ref={tabs}
+                  marginTop={keyboardVisible ? -223 : 0}
+                >
+                  <Tabs setSelectedRoute={handleTabSelection} selectedRoute={route} />
                 </Container>
-            <Container
-              ref={bar}
-              alignSelf={"center"}
-              alignItems={"center"}
-              height={25}
-              marginLeft={route === Route.TV ? -400 : 0}
-            >
-              <BottomBar
-                environmentControls={true}
-                setEnvironmentValue={setImmersionLevel}
-                environmentValue={immersionLevel}
-                handleReference={handle}
-                dragHandlers={{
-                  onPointerDown: handlePointerDown,
-                  onPointerUp: handlePointerUp,
-                  onPointerMove: handlePointerMove
+                <Container flexDirection={"column"} height={"auto"} alignItems={"center"}>
+                  <Container height={"auto"}>
+                    {route === "HOME" && <Home />}
+                    {route === "TV" && <Tv viewRef={view} handleRef={bar} tabsRef={tabs} />}
+                    {route === "GUIDE" && <GuideView viewRef={view} handleRef={handle} tabsRef={tabs} />}
+                    {route === "SETTINGS" && <SettingsView />}
+                  </Container>
+                  <Container
+                    ref={bar}
+                    alignSelf={"center"}
+                    alignItems={"center"}
+                    height={25}
+                    marginLeft={route === Route.TV ? -400 : 0}
+                  >
+                    <BottomBar
+                      environmentControls={true}
+                      setEnvironmentValue={setImmersionLevel}
+                      environmentValue={immersionLevel}
+                      handleReference={handle}
+                      dragHandlers={{
+                        onPointerDown: handlePointerDown,
+                        onPointerUp: handlePointerUp,
+                        onPointerMove: handlePointerMove
 
-                }}
-              />
-            </Container>
-            {keyboardVisible && <Container
-              alignSelf={"center"}
-              alignItems={"center"}
-              marginTop={30}
-              transformRotateX={-20}
-            >
-              <KeyboardUI />
-            </Container>}
-            </Container>
-            </Container>
-          </Root>
-        </group>
-        <Environment immersionLevel={immersionLevel} nightMode={biTheme === BiTheme.DARK} />
-        <XROrigin position={[0, 0, 4]}>
-          {/*<DefaultXRController />  This can enable us to have custom pointer options, TODO. Currently causes errors */}
-        </XROrigin>
-        <color attach="background" args={["#bfbebe"]} />
-      </XR>
+                      }}
+                    />
+                  </Container>
+                  {keyboardVisible && <Container
+                    alignSelf={"center"}
+                    alignItems={"center"}
+                    marginTop={30}
+                    transformRotateX={-20}
+                  >
+                    <KeyboardUI />
+                  </Container>}
+                </Container>
+              </Container>
+            </Root>
+          </group>
+          <Environment immersionLevel={immersionLevel} nightMode={biTheme === BiTheme.DARK} />
+          <Locomotion position={[0, 0, 4]} />
+          <color attach="background" args={["#bfbebe"]} />
+        </XR>
       </Canvas>
     </div>
   );
